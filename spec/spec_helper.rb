adbac964318e4bf48703e697c5420175349bdddc
reppard <reppardwalker@gmail.com>
2016-02-07 12:06:49 -0500

    feat(spec_helper.rb): add pry and mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'vaderc'
+require 'pry'
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+  end
+end
