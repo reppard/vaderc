15c56c069527ae7540947bc5f72a193eb89e1d28
reppard <reppardwalker@gmail.com>
2016-02-08 20:59:53 -0500

    fix(vaderc.rb): fix rubocop violations

--- lib/vaderc.rb
+++ lib/vaderc.rb
@@ -1,10 +1,11 @@
-require "vaderc/version"
-require "vaderc/configuration"
-require "vaderc/socket_wrapper"
+require 'vaderc/version'
+require 'vaderc/configuration'
+require 'vaderc/socket_wrapper'
 
+# Vaderc Module
 module Vaderc
   class << self
-    def run opts = {}
+    def run(opts = {})
       @config ||= Vaderc::Configuration.new( opts )
     end
   end
